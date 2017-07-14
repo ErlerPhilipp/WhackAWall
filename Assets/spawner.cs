1bc0b51256e9f850db8fe49c140895a1881ebff2
VRVIS\erler <erler@vrvis.at>
2017-07-14 22:02:25 +0200

    basic stuff

---
+++ Assets/spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+    public GameObject hitSphere;
+    public int spawnNum = 0;
+    int maxNum = 3;
+    public int leftscore = 0;
+    public int rightscore = 0;
+
+
+    void spawn()
+    {
+        for(int i = spawnNum; i < maxNum; i++)
+        {
+            Vector3 HitSpherePos = new Vector3(this.transform.position.x + Random.Range(-this.transform.lossyScale.x/2, this.transform.lossyScale.x/2),
+                                               this.transform.position.y + Random.Range(-this.transform.lossyScale.y/2, this.transform.lossyScale.y/2),
+                                               this.transform.position.z + Random.Range(-this.transform.lossyScale.z/2, this.transform.lossyScale.z/2));
+            Instantiate(hitSphere, HitSpherePos, Quaternion.identity);
+            spawnNum++;
+        }
+    }
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        spawn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spawn();
+    }
+}
---
+++ Assets/touch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+
+public class touch : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    public GameObject SpawnArea;
+    int counter = 0;
+    
+    // Use this for initialization
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        if(counter > 0)
+        {
+            SteamVR_Controller.Input((int)trackedObj.index).TriggerHapticPulse(2000);
+            counter--;
+        }
+	}
+
+    public void OnTriggerStay(Collider other)
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+            spawner spawn = SpawnArea.GetComponent("spawner") as spawner;
+            spawn.spawnNum--;
+            if (trackedObj.tag == "left")
+            {
+                counter = 45;
+                spawn.leftscore++;
+                Debug.Log("Linker Score: " + spawn.leftscore);
+            }
+            else
+            {
+                counter = 45;
+                spawn.rightscore++;
+                Debug.Log("Rechter Score: "+ spawn.rightscore);
+            }
+
+            
+        }
+
+    }
+}
